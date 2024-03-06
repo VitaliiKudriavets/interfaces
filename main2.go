70db28de07c2de60c3bfe70c906b44bc5293357e
Vitalii Kudriavets <14039142+VitaliiKudriavets@users.noreply.github.com>
2024-03-06 23:35:33 +0200

    Implement User structure and interface in main2.go

--- main2.go
+++ main2.go
@@ -1,5 +1,33 @@
 package main
 
+var _ User = &user{}
+
+type user struct {
+	FIO, Address, Phone string
+}
+
+func (u *user) ChangeFIO(newFIO string) {
+	u.FIO = newFIO
+}
+
+func (u *user) ChangeAddress(newAddress string) {
+	u.Address = newAddress
+}
+
+type User interface {
+	ChangeFIO(newFIO string)
+	ChangeAddress(newAddress string)
+}
+
+func newUser(fio, address, phone string) User {
+	u := user{
+		FIO:     fio,
+		Address: address,
+		Phone:   phone,
+	}
+	return &u
+}
+
 func main() {
 
 }
